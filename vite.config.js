9c017abda85b4800017bfe79179ee64b3a15a49c
rifky himawan <rifky1401@student.abudzar.sch.id>
2026-06-13 15:14:47 +0700

    add doc

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    
   ],
-  base: '/ASAT-IT-10B/',
+  base: "/ASAT-IT-10B/",
+  build: {
+    outDir: 'docs'
+  }
 })
